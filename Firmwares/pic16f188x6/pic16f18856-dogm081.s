3a7c4ffc27bbdbae480de5def47a3d81aea69395
Tronix I/O <spesant@gmail.com>
2024-03-16 16:17:07 +0100

    Update pic16f18856-dogm081.s

--- Firmwares/pic16f188x6/pic16f18856-dogm081.s
+++ Firmwares/pic16f188x6/pic16f18856-dogm081.s
@@ -67,57 +67,57 @@ psect cstackCOMMON,global,class=COMMON,space=1,delta=1,noexec
 #define	BANK18	    0x12
 #define	BANK19	    0x13
 #define	BANK29	    0x1D
-#define	BANK30	    0x1E
+#define	BANK30      0x1E
 ; SFR - Special Function Registers.
 ; SFR STATUS Bits.
 #define	C	    0x0
 #define	Z	    0x2
 ; SFR MSSP - SSPxCON1 Bits.
 #define	BF	    0x0
-#define SSPEN	    0x5
+#define SSPEN	0x5
 
 ; USER Definitions.
 ; Debug.
-#define LED_DEBUG   LATA, 0x6
+#define LED_DEBUG                                           LATA, 0x6
 ; ST7036 Commands Port.
-#define ST7036_RS   LATC, 0x2
+#define ST7036_RS                                           LATC, 0x2
 ; ST7036 Instruction Table IS2=0, IS1=0.
-#define ST7036_CLEAR_DISPLAY                               0x01
-#define ST7036_RETURN_HOME                                 0x02
-#define ST7036_ENTRY_MODE_SET_DDRAM_DECREMENT_NOSHIFT      0x04
-#define ST7036_ENTRY_MODE_SET_DDRAM_DECREMENT_SHIFT_RIGHT  0x05
-#define ST7036_ENTRY_MODE_SET_DDRAM_INCREMENT_NOSHIFT      0x06
-#define ST7036_ENTRY_MODE_SET_DDRAM_INCREMENT_SHIFT_LEFT   0x07
-#define ST7036_DISPLAY_OFF                                 0x08
-#define ST7036_DISPLAY_ON_CURSOR_OFF                       0x0C
-#define ST7036_DISPLAY_ON_CURSOR_ON_NOBLINK                0x0E
-#define ST7036_DISPLAY_ON_CURSOR_ON_BLINK                  0x0F
-#define ST7036_DISPLAY_CURSOR_SHIFT_LEFT                   0x10
-#define ST7036_DISPLAY_CURSOR_SHIFT_RIGHT                  0x14
-#define ST7036_DISPLAY_DISPLAY_SHIFT_LEFT                  0x18
-#define ST7036_DISPLAY_DISPLAY_SHIFT_RIGH                  0x1C
-#define ST7036_FUNCTION_SET_4_BIT_ONE_LINE_FONT5x8         0x20
-#define ST7036_FUNCTION_SET_4_BIT_ONE_LINE_FONT5x8_IS1     0x21
-#define ST7036_FUNCTION_SET_4_BIT_ONE_LINE_FONT5x8_IS2     0x22
-#define ST7036_FUNCTION_SET_4_BIT_TWO_LINE_FONT5x8         0x28
-#define ST7036_FUNCTION_SET_4_BIT_ONE_LINE_DHFONT5x8       0x24
-#define ST7036_FUNCTION_SET_4_BIT_TWO_LINE_DHFONT5x8       0x2C
-#define ST7036_FUNCTION_SET_8_BIT_ONE_LINE_FONT5x8         0x30
-#define ST7036_FUNCTION_SET_8_BIT_ONE_LINE_FONT5x8_IS1     0x31
-#define ST7036_FUNCTION_SET_8_BIT_ONE_LINE_FONT5x8_IS2     0x32
-#define ST7036_FUNCTION_SET_8_BIT_ONE_LINE_DHFONT5x8       0x34
-#define ST7036_FUNCTION_SET_8_BIT_TWO_LINE_FONT5x8         0x38
-#define ST7036_FUNCTION_SET_8_BIT_TWO_LINE_DHFONT5x8       0x3C
-#define ST7036_SET_ICON_RAM_ADDRESS                        0X40
-#define ST7036_SET_CGRAM_ADDRESS                           0x40
-#define ST7036_DDRAM_ADDRESS_FIRST_LINE                    0x80
+#define ST7036_CLEAR_DISPLAY                                0x01
+#define ST7036_RETURN_HOME                                  0x02
+#define ST7036_ENTRY_MODE_SET_DDRAM_DECREMENT_NOSHIFT       0x04
+#define ST7036_ENTRY_MODE_SET_DDRAM_DECREMENT_SHIFT_RIGHT   0x05
+#define ST7036_ENTRY_MODE_SET_DDRAM_INCREMENT_NOSHIFT       0x06
+#define ST7036_ENTRY_MODE_SET_DDRAM_INCREMENT_SHIFT_LEFT    0x07
+#define ST7036_DISPLAY_OFF                                  0x08
+#define ST7036_DISPLAY_ON_CURSOR_OFF                        0x0C
+#define ST7036_DISPLAY_ON_CURSOR_ON_NOBLINK                 0x0E
+#define ST7036_DISPLAY_ON_CURSOR_ON_BLINK                   0x0F
+#define ST7036_DISPLAY_CURSOR_SHIFT_LEFT                    0x10
+#define ST7036_DISPLAY_CURSOR_SHIFT_RIGHT                   0x14
+#define ST7036_DISPLAY_DISPLAY_SHIFT_LEFT                   0x18
+#define ST7036_DISPLAY_DISPLAY_SHIFT_RIGH                   0x1C
+#define ST7036_FUNCTION_SET_4_BIT_ONE_LINE_FONT5x8          0x20
+#define ST7036_FUNCTION_SET_4_BIT_ONE_LINE_FONT5x8_IS1      0x21
+#define ST7036_FUNCTION_SET_4_BIT_ONE_LINE_FONT5x8_IS2      0x22
+#define ST7036_FUNCTION_SET_4_BIT_TWO_LINE_FONT5x8          0x28
+#define ST7036_FUNCTION_SET_4_BIT_ONE_LINE_DHFONT5x8        0x24
+#define ST7036_FUNCTION_SET_4_BIT_TWO_LINE_DHFONT5x8        0x2C
+#define ST7036_FUNCTION_SET_8_BIT_ONE_LINE_FONT5x8          0x30
+#define ST7036_FUNCTION_SET_8_BIT_ONE_LINE_FONT5x8_IS1      0x31
+#define ST7036_FUNCTION_SET_8_BIT_ONE_LINE_FONT5x8_IS2      0x32
+#define ST7036_FUNCTION_SET_8_BIT_ONE_LINE_DHFONT5x8        0x34
+#define ST7036_FUNCTION_SET_8_BIT_TWO_LINE_FONT5x8          0x38
+#define ST7036_FUNCTION_SET_8_BIT_TWO_LINE_DHFONT5x8        0x3C
+#define ST7036_SET_ICON_RAM_ADDRESS                         0X40
+#define ST7036_SET_CGRAM_ADDRESS                            0x40
+#define ST7036_DDRAM_ADDRESS_FIRST_LINE                     0x80
 ; ST7036 Instruction Table IS2=0, IS1=1.
-#define ST7036_BIAS_SET_1_5				    0x14
-#define ST7036_BIAS_SET_1_5_3_LINE			    0x15
-#define ST7036_BIAS_SET_1_4				    0x1C
-#define ST7036_BIAS_SET_1_4_3_LINE			    0x1D
-#define ST7036_POWER_ICON_OFF_BOOST_OFF_NO_CONTRAST	    0x50
-#define ST7036_POWER_ICON_ON_BOOST_OFF_NO_CONTRAST	    0x58
+#define ST7036_BIAS_SET_1_5                                 0x14
+#define ST7036_BIAS_SET_1_5_3_LINE                          0x15
+#define ST7036_BIAS_SET_1_4                                 0x1C
+#define ST7036_BIAS_SET_1_4_3_LINE                          0x1D
+#define ST7036_POWER_ICON_OFF_BOOST_OFF_NO_CONTRAST         0x50
+#define ST7036_POWER_ICON_ON_BOOST_OFF_NO_CONTRAST          0x58
 #define ST7036_POWER_ICON_OFF_BOOST_ON_CONTRAST_MSB_0	    0x54
 #define ST7036_POWER_ICON_OFF_BOOST_ON_CONTRAST_MSB_1	    0x55
 #define ST7036_POWER_ICON_OFF_BOOST_ON_CONTRAST_MSB_2	    0x56
@@ -126,37 +126,37 @@ psect cstackCOMMON,global,class=COMMON,space=1,delta=1,noexec
 #define ST7036_POWER_ICON_ON_BOOST_ON_CONTRAST_MSB_1	    0x5D
 #define ST7036_POWER_ICON_ON_BOOST_ON_CONTRAST_MSB_2	    0x5E
 #define ST7036_POWER_ICON_ON_BOOST_ON_CONTRAST_MSB_3	    0x5F
-#define ST7036_FOLLOWER_CONTROL_OFF			    0x60
-#define ST7036_FOLLOWER_CONTROL_ON_RAB_0		    0x68
-#define ST7036_FOLLOWER_CONTROL_ON_RAB_1		    0x69
-#define ST7036_FOLLOWER_CONTROL_ON_RAB_2		    0x6A
-#define ST7036_FOLLOWER_CONTROL_ON_RAB_3		    0x6B
-#define ST7036_FOLLOWER_CONTROL_ON_RAB_4		    0x6C
-#define ST7036_FOLLOWER_CONTROL_ON_RAB_5		    0x6D
-#define ST7036_FOLLOWER_CONTROL_ON_RAB_6		    0x6E
-#define ST7036_FOLLOWER_CONTROL_ON_RAB_7		    0x6F
-#define ST7036_CONTRAST_LSB_0				    0x70
-#define ST7036_CONTRAST_LSB_1				    0x71
-#define ST7036_CONTRAST_LSB_2				    0x72
-#define ST7036_CONTRAST_LSB_3				    0x73
-#define ST7036_CONTRAST_LSB_4				    0x74
-#define ST7036_CONTRAST_LSB_5				    0x75
-#define ST7036_CONTRAST_LSB_6				    0x76
-#define ST7036_CONTRAST_LSB_7				    0x77
-#define ST7036_CONTRAST_LSB_8				    0x78
-#define ST7036_CONTRAST_LSB_9				    0x79
-#define ST7036_CONTRAST_LSB_10				    0x7A
-#define ST7036_CONTRAST_LSB_11				    0x7B
-#define ST7036_CONTRAST_LSB_12				    0x7C
-#define ST7036_CONTRAST_LSB_13				    0x7D
-#define ST7036_CONTRAST_LSB_14				    0x7E
-#define ST7036_CONTRAST_LSB_15				    0x7F
-#define	ST7036_INSTRUCTION_DELAY			    2
-#define ST7036_CLEAR_DISPLAY_DELAY			    1
-#define ST7036_INITIALIZATION_DELAY			    30
+#define ST7036_FOLLOWER_CONTROL_OFF                         0x60
+#define ST7036_FOLLOWER_CONTROL_ON_RAB_0                    0x68
+#define ST7036_FOLLOWER_CONTROL_ON_RAB_1                    0x69
+#define ST7036_FOLLOWER_CONTROL_ON_RAB_2                    0x6A
+#define ST7036_FOLLOWER_CONTROL_ON_RAB_3                    0x6B
+#define ST7036_FOLLOWER_CONTROL_ON_RAB_4                    0x6C
+#define ST7036_FOLLOWER_CONTROL_ON_RAB_5                    0x6D
+#define ST7036_FOLLOWER_CONTROL_ON_RAB_6                    0x6E
+#define ST7036_FOLLOWER_CONTROL_ON_RAB_7                    0x6F
+#define ST7036_CONTRAST_LSB_0                               0x70
+#define ST7036_CONTRAST_LSB_1                               0x71
+#define ST7036_CONTRAST_LSB_2                               0x72
+#define ST7036_CONTRAST_LSB_3                               0x73
+#define ST7036_CONTRAST_LSB_4                               0x74
+#define ST7036_CONTRAST_LSB_5                               0x75
+#define ST7036_CONTRAST_LSB_6                               0x76
+#define ST7036_CONTRAST_LSB_7                               0x77
+#define ST7036_CONTRAST_LSB_8                               0x78
+#define ST7036_CONTRAST_LSB_9                               0x79
+#define ST7036_CONTRAST_LSB_10                              0x7A
+#define ST7036_CONTRAST_LSB_11                              0x7B
+#define ST7036_CONTRAST_LSB_12                              0x7C
+#define ST7036_CONTRAST_LSB_13                              0x7D
+#define ST7036_CONTRAST_LSB_14                              0x7E
+#define ST7036_CONTRAST_LSB_15                              0x7F
+#define	ST7036_INSTRUCTION_DELAY                            2
+#define ST7036_CLEAR_DISPLAY_DELAY                          1
+#define ST7036_INITIALIZATION_DELAY                         30
 ; ST7036 Instruction Table IS2=1, IS1=0.
-#define ST7036_DOUBLE_HEIGHT_FONT_COM9_COM24               0x10
-#define ST7036_DOUBLE_HEIGHT_FONT_COM1_COM16               0x11
+#define ST7036_DOUBLE_HEIGHT_FONT_COM9_COM24                0x10
+#define ST7036_DOUBLE_HEIGHT_FONT_COM1_COM16                0x11
 
 ; Reset Vector.
 PSECT reset_vec,global,class=CODE,merge=1,delta=2
@@ -279,7 +279,7 @@ main:
     MOVLW   0x14
     MOVWF   RC3PPS  ; MSSP1.SCK.
     MOVLW   0x15
-    MOVWF   RC4PPS  ; MSSP1.SDO.
+    MOVWF   RC4PPS  ; MSSP1.MISO.
 
     ; MSSP Settings.
     ; MSSP1 SPI Master - Mode 0.
@@ -300,7 +300,6 @@ main:
     CALL    dogm081_writeTRONIX
 
 loop:
-;    BRA	    loop
     BRA	    $
 
 ; Interrupt Service Routine.
